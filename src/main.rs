0b01f73e43497daf7f8e1fad7db60af302e085b1
FizzyApple12 <github@fizzyapple12.com>
2024-09-11 15:45:08 -0400

    16 channel software PWM for the ESP32

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 #![no_std]
 #![no_main]
 
-use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, signal::Signal};
-use embassy_time::{Duration, Ticker};
-use embedded_hal::digital::OutputPin;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
     cpu_control::{CpuControl, Stack},
-    get_core,
-    gpio::{Io, Level, Output},
+    gpio::{AnyOutput, Io, Level},
     interrupt::Priority,
     peripherals::Peripherals,
     prelude::*,
@@ -17,6 +13,7 @@ use esp_hal::{
     timer::timg::TimerGroup,
 };
 use esp_hal_embassy::InterruptExecutor;
+use sign_firmware::{leds_software_pwm, Leds};
 use static_cell::StaticCell;
 
 extern crate alloc;
@@ -36,44 +33,6 @@ fn init_heap() {
 
 static mut APP_CORE_STACK: Stack<8192> = Stack::new();
 
-/// Waits for a message that contains a duration, then flashes a led for that
-/// duration of time.
-#[embassy_executor::task]
-async fn control_led(
-    mut led: impl OutputPin + 'static,
-    control: &'static Signal<CriticalSectionRawMutex, bool>,
-) {
-    esp_println::println!("Starting control_led() on core {}", get_core() as usize);
-    loop {
-        if control.wait().await {
-            esp_println::println!("LED on");
-            let _ = led.set_low();
-        } else {
-            esp_println::println!("LED off");
-            let _ = led.set_high();
-        }
-    }
-}
-
-/// Sends periodic messages to control_led, enabling or disabling it.
-#[embassy_executor::task]
-async fn enable_disable_led(control: &'static Signal<CriticalSectionRawMutex, bool>) {
-    esp_println::println!(
-        "Starting enable_disable_led() on core {}",
-        get_core() as usize
-    );
-    let mut ticker = Ticker::every(Duration::from_secs(1));
-    loop {
-        esp_println::println!("Sending LED on");
-        control.signal(true);
-        ticker.next().await;
-
-        esp_println::println!("Sending LED off");
-        control.signal(false);
-        ticker.next().await;
-    }
-}
-
 #[entry]
 fn main() -> ! {
     init_heap();
@@ -84,25 +43,39 @@ fn main() -> ! {
 
     let system = SystemControl::new(peripherals.SYSTEM);
     let sw_ints = system.software_interrupt_control;
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
-    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let timg0 = TimerGroup::new(peripherals.TIMG1, &clocks);
     esp_hal_embassy::init(&clocks, timg0.timer0);
 
-    let _init = esp_wifi::initialize(
-        esp_wifi::EspWifiInitFor::Wifi,
-        timg0.timer1,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-        &clocks,
-    )
-    .unwrap();
+    // let _init = esp_wifi::initialize(
+    //     esp_wifi::EspWifiInitFor::Wifi,
+    //     timg0.timer1,
+    //     esp_hal::rng::Rng::new(peripherals.RNG),
+    //     peripherals.RADIO_CLK,
+    //     &clocks,
+    // )
+    // .unwrap();
 
     let mut cpu_control = CpuControl::new(peripherals.CPU_CTRL);
 
-    static LED_CTRL: StaticCell<Signal<CriticalSectionRawMutex, bool>> = StaticCell::new();
-    let led_ctrl_signal = &*LED_CTRL.init(Signal::new());
-
-    let led = Output::new(io.pins.gpio0, Level::Low);
+    let leds = [
+        AnyOutput::new(io.pins.gpio1, Level::Low),
+        AnyOutput::new(io.pins.gpio2, Level::Low),
+        AnyOutput::new(io.pins.gpio4, Level::Low),
+        AnyOutput::new(io.pins.gpio5, Level::Low),
+        AnyOutput::new(io.pins.gpio6, Level::Low),
+        AnyOutput::new(io.pins.gpio7, Level::Low),
+        AnyOutput::new(io.pins.gpio8, Level::Low),
+        AnyOutput::new(io.pins.gpio9, Level::Low),
+        AnyOutput::new(io.pins.gpio10, Level::Low),
+        AnyOutput::new(io.pins.gpio11, Level::Low),
+        AnyOutput::new(io.pins.gpio12, Level::Low),
+        AnyOutput::new(io.pins.gpio13, Level::Low),
+        AnyOutput::new(io.pins.gpio14, Level::Low),
+        AnyOutput::new(io.pins.gpio15, Level::Low),
+        AnyOutput::new(io.pins.gpio16, Level::Low),
+    ];
 
     static EXECUTOR_CORE_1: StaticCell<InterruptExecutor<1>> = StaticCell::new();
     let executor_core1 = InterruptExecutor::new(sw_ints.software_interrupt1);
@@ -112,7 +85,7 @@ fn main() -> ! {
         .start_app_core(unsafe { &mut *addr_of_mut!(APP_CORE_STACK) }, move || {
             let spawner = executor_core1.start(Priority::Priority1);
 
-            spawner.spawn(control_led(led, led_ctrl_signal)).ok();
+            spawner.spawn(leds_software_pwm(leds)).ok();
 
             // Just loop to show that the main thread does not need to poll the executor.
             loop {}
@@ -124,7 +97,8 @@ fn main() -> ! {
     let executor_core0 = EXECUTOR_CORE_0.init(executor_core0);
 
     let spawner = executor_core0.start(Priority::Priority1);
-    spawner.spawn(enable_disable_led(led_ctrl_signal)).ok();
+
+    let leds = Leds::create();
 
     // Just loop to show that the main thread does not need to poll the executor.
     loop {}
